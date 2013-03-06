c5a53b11a6b5380e62c36128e2b5564ce4c67a77
Steven Deutscher-Kobayashi <sdeutscherkobayashi@gmail.com>
2013-03-06 02:37:20 -0500

    Adding ArrayDeclExp

---
+++ Assignments-3..5/src/compiler488/semantics/ArrayDeclarationException.java
@@ -0,0 +1,7 @@
+package compiler488.semantics;
+
+public class ArrayDeclarationException extends RuntimeException {
+  public ArrayDeclarationException(String message) {
+    super(message);
+  }
+}
